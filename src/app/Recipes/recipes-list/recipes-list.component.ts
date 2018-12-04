c95fc1b5fffc8783f5bc6da89b860b37720f4ed8
Tina <martina.schina@icloud.com>
2018-12-04 17:23:48 +0100

    Lista ngFor recipes

--- src/app/Recipes/recipes-list/recipes-list.component.ts
+++ src/app/Recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes.models';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+ricetta: Recipe = new Recipe(
+    'Lasagne',
+    'Lorem Ipsum',
+    'https://cc-media-foxit.fichub.com/image/fox-it-life/1fc0fb34-c6b2-43b1-b9c4-abe0744b8cfa/lasagna-classica-maxw-654.jpg'
+  );
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Lasagne',
+      'Lorem Ipsum',
+      'https://cc-media-foxit.fichub.com/image/fox-it-life/1fc0fb34-c6b2-43b1-b9c4-abe0744b8cfa/lasagna-classica-maxw-654.jpg'
+    ),
+
+    new Recipe(
+      'Carbonara',
+      'Lorem Ipsum',
+      'https://www.gustissimo.it/articoli/ricette/pasta-salumi/spaghetti-alla-carbonara.jpg'
+    ),
+
+    new Recipe(
+      'Amatriciana',
+      'Lorem Ipsum',
+      'http://www.strettoweb.com/wp-content/uploads/2016/08/31-8-Amatriciana-in-Piazza-Portosalvo-800x600.jpg'
+    )
+  ];
 
   constructor() { }
 
